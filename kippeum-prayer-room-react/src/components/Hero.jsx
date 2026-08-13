f5b74b9b78509f40c001f6e0e862eb535a3a77b2
yko79135 <yko79135@gmail.com>
2026-08-13 10:32:07 +0900

    Allow prayer room title to wrap

--- kippeum-prayer-room-react/src/components/Hero.jsx
+++ kippeum-prayer-room-react/src/components/Hero.jsx
@@ -35,11 +35,7 @@ export function Hero({ isAdminMode, onAdminClick, adminButtonRef }) {
 
       <div className="heroText">
         <span className="heroKicker">KIPPEUM PRAYER ROOM</span>
-        <h1>옥상 “잠근동산” 예약 시스템</h1>
-        <p>
-          하나님은 우리의 기도를 들으십니다.
-        </p>
-        <span className="heroBlessing">조용한 기도의 자리로 초대합니다</span>
+        <h1>옥상 “잠근동산” 기도실 예약 시스템</h1>
       </div>
     </section>
   );
